04c12b4890d0e32977328cad5d0426ea513012ae
Robert Peteuil <robert@peteuil.com>
2018-03-16 09:20:46 -0700

    add python 3.6 version

---
+++ python3.6/app/main.py
@@ -0,0 +1,12 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hello World from Python 3.6 Flask App in Docker container with \
+     	     MS SQL Driver, uWSGI and Nginx"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True, port=80)
---
+++ python3.6/build-image.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker build -t robpco/mssql-flask-uwsgi-nginx:latest .
