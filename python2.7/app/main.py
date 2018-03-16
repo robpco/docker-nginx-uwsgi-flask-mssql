97e417b0fde5e604806bc47d375a03c5364a2cf2
Robert Peteuil <robert@peteuil.com>
2018-03-16 09:20:33 -0700

    adjust base python app

--- python2.7/app/main.py
+++ python2.7/app/main.py
@@ -4,9 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World from Python 2.7 via Flask in Docker container with \
-     	    uWSGI, Nginx and MS Sql ODBC Driver"
-
+    return "Hello World from Python 2.7 Flask App in Docker container with \
+     	     MS SQL Driver, uWSGI and Nginx"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=80)
